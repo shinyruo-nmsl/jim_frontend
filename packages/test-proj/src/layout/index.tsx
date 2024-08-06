2eee222d942222bdb9517f7ac660847c2b770281
jim <jiminhu@trip.com>
2024-08-06 10:12:09 +0800

    test

---
+++ packages/test-proj/src/layout/index.tsx
@@ -0,0 +1,96 @@
+import { NavBar, SideBar, Dialog } from "antd-mobile";
+import { MoreOutline, ExclamationCircleFill } from "antd-mobile-icons";
+import Router from "@/router";
+import { Suspense, useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { WebContext, WebService } from "web-common";
+import { Http } from "proj-util";
+
+const {
+  User: { useUserLoginInfo },
+} = WebContext;
+
+const {
+  Router: { useUserRouter },
+} = WebService;
+
+function Layout() {
+  const userInfo = useUserLoginInfo();
+
+  const { leafRoutes, curRouteTrace, navigate, navigateSubRoute } =
+    useUserRouter(Router, userInfo.role);
+
+  const [sideBarVisible, setSideBarVisible] = useState(false);
+
+  const right = (
+    <div className="text-24 flex justify-end">
+      <MoreOutline onClick={() => setSideBarVisible(true)} />
+    </div>
+  );
+
+  const title = curRouteTrace[curRouteTrace.length - 1]?.label;
+
+  useEffect(() => {
+    const handler = () => {
+      Dialog.alert({
+        header: (
+          <ExclamationCircleFill
+            style={{
+              fontSize: 64,
+              color: "var(--adm-color-warning)",
+            }}
+          />
+        ),
+        content: (
+          <>
+            <div>使用此功能需要登录</div>
+          </>
+        ),
+        onConfirm: () => {
+          navigate("/login");
+        },
+      });
+    };
+
+    Http.HttpEventEmitter.registHandler("code_401", handler);
+
+    return () => Http.HttpEventEmitter.removeHandler("code_401", handler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <NavBar
+        className="w-full flex-none"
+        right={right}
+        onBack={() => navigate(-1)}
+      >
+        {title}
+      </NavBar>
+      <div
+        className="w-full flex-auto overflow-y-auto overflow-x-hidden"
+        onClick={() => setSideBarVisible(false)}
+      >
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </div>
+      {sideBarVisible && (
+        <SideBar
+          className="fixed right-0 top-0 h-full w-[40%] z-prioty"
+          activeKey={curRouteTrace.map((item) => item.path).join("/")}
+          onChange={(key) => {
+            setSideBarVisible(false);
+            navigateSubRoute(key.split("/"), "path");
+          }}
+        >
+          {leafRoutes.map((item) => (
+            <SideBar.Item key={item.paths.join("/")} title={item.label} />
+          ))}
+        </SideBar>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ packages/test-proj/vite.config.ts
@@ -0,0 +1,72 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
+import checker from "vite-plugin-checker";
+import eslintPlugin from "vite-plugin-eslint";
+import { visualizer } from "rollup-plugin-visualizer";
+
+const isProd = process.env.NODE_ENV === "production";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: isProd ? "./" : "/",
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+    }),
+    {
+      ...legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+      apply: "build",
+    },
+    // checker({ typescript: true }),
+    eslintPlugin(),
+  ],
+  server: {
+    port: 8888,
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@web": resolve(__dirname, "../web-common/src"),
+    },
+    extensions: [".js", ".json", ".ts", ".tsx", ".jsx"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("markdown-it")) {
+            return "markdown-it";
+          }
+          if (id.includes("highlight.js")) {
+            return "highlight";
+          }
+          if (id.includes("pptxgenjs")) {
+            return "pptxgenjs";
+          }
+          if (id.includes("pptxtojson")) {
+            return "pptxtojson";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true, // 删除所有console语句
+        pure_funcs: ["console.log"], // 删除console.log语句
+      },
+    },
+    assetsInlineLimit: 0, // 设置为 0 禁用内联
+
+    minify: false, // 禁用压缩
+    sourcemap: true, // 生成 source map 文件，便于调试
+  },
+});
