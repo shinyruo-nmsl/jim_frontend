fd33d81de07f5dfdc039116ed0cb39d1186b3f18
jim <871939254@qq.com>
2024-08-11 12:30:43 +0800

    重新构建

--- packages/ssr-proj/next.config.mjs
+++ packages/ssr-proj/next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/ssr" : "",
+  basePath: isProd ? "/ssr/" : "/",
 };
 
 export default nextConfig;
