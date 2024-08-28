5d1aea05210b0981864ba7ee5d2990c735267330
jim <jiminhu@trip.com>
2024-08-28 17:37:33 +0800

    bugfix

--- packages/ssr-proj/app/blog/[id]/page.tsx
+++ packages/ssr-proj/app/blog/[id]/page.tsx
@@ -2,13 +2,14 @@
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeStarryNight from 'rehype-starry-night'
 import rehypeImgSize from 'rehype-img-size';
 import remarkGfm from 'remark-gfm';
 import remarkToc from 'remark-toc';
 import { unified } from 'unified'
 import 'github-markdown-css'
 import { fetchGetBlogArticleByID, fetchGetBlogArticles } from "@/app/blog/api";
+
 
 export async function generateStaticParams() {
     const articles = await fetchGetBlogArticles()
@@ -22,12 +23,14 @@ export default async function BlogArticlePage({ params }: { params: { id: string
     const article = await fetchGetBlogArticleByID(params.id);
     const markdownText = await fetch(article.link).then(res => res.text());
     const file = await unified()
-        .use(remarkParse)
+        .use(remarkParse, { fragment: true })
         .use(remarkRehype)
         .use(remarkToc)
         .use(remarkGfm)
         .use(rehypeImgSize)
-        .use(rehypePrettyCode)
+        .use(rehypeStarryNight)
+        // .use(rehypeSanitize)
+        // .use(rehypeHighlight)
         .use(rehypeStringify).process(markdownText);
 
 
