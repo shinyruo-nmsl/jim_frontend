8645cca3b453f505091ca867c5b50c653b08a859
jim <jiminhu@trip.com>
2024-08-28 18:52:54 +0800

    打包bug

--- packages/ssr-proj/next.config.mjs
+++ packages/ssr-proj/next.config.mjs
@@ -8,16 +8,13 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   assetPrefix: isProd ? "/ssr" : "",
   distDir: "build",
+  basePath: isProd ? "/ssr" : "",
   // experimental: {
   //   ppr: true,
   // },
-  experimental: {
-    serverComponentsExternalPackages: ["vscode-oniguruma", "shiki"],
-  },
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": resolve(__dirname, "src"),
       "@web": resolve(__dirname, "../web-common/src"),
       "@util": resolve(__dirname, "../proj-util/src"),
       "@type": resolve(__dirname, "../proj-type/src"),
